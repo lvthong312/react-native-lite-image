2818154f9c916de6e930c3407a28e40a30964422
Thong Luong <lvthong312@gmail.com>
2025-10-26 17:47:01 +0700

    feat: support for require

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,20 @@
-export { default as LiteImage } from './LiteImageViewViewNativeComponent';
+import { Image } from 'react-native';
+import type { NativeProps } from './LiteImageViewViewNativeComponent';
+
+import LiteImageView from './LiteImageViewViewNativeComponent';
 export * from './LiteImageViewViewNativeComponent';
+
+export const LiteImage = ({ source, ...rest }: NativeProps) => {
+  let uri = null;
+
+  if (typeof source === 'number') {
+    const resolved = Image.resolveAssetSource(source);
+    uri = resolved?.uri ?? null;
+  } else if (typeof source === 'object' && source?.uri) {
+    uri = source.uri;
+  } else if (typeof source === 'string') {
+    uri = source;
+  }
+
+  return <LiteImageView source={{ uri: uri || '' }} {...rest} />;
+};
